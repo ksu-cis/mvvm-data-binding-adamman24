b04f4cf3e587d43a66327141a0cb636614a63578
Adam Winter <adamwinter22@ksu.edu>
2020-04-03 21:23:18 -0500

    Cash Register

---
+++ MVVMDataBinding/BillControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for CoinControl.xaml
+    /// </summary>
+    public partial class BillControl : UserControl
+    {
+        /// <summary>
+        /// dependency prop for denomination
+        /// </summary>
+        public static readonly DependencyProperty DenominationProperty =
+            DependencyProperty.Register("Denomination", typeof(Bills),
+                typeof(BillControl),
+                new PropertyMetadata(Bills.One));
+
+        /// <summary>
+        /// denomination of coin
+        /// </summary>
+        public Bills Denomination
+        {
+            get { return (Bills)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        /// <summary>
+        /// dependency prop for quantity
+        /// </summary>
+        public static readonly DependencyProperty QuantityProperty =
+            DependencyProperty.Register(
+                "Quantity",
+                typeof(int),
+                typeof(BillControl),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// get set quantity of coin
+        /// </summary>
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set => SetValue(QuantityProperty, value);
+        }
+
+        public BillControl()
+        {
+            InitializeComponent();
+        }
+
+        public void OnIncreaseClicked(object sender, RoutedEventArgs e)
+        {
+            Quantity++;
+        }
+
+        public void OnDecreaseClicked(object sender, RoutedEventArgs e)
+        {
+            Quantity--;
+        }
+    }
+}
